8497ad3759ba11176b751e0083f0ca3bbaacfb72
gt005 <karimhamid@yandex.ru>
2024-06-18 19:09:56 +0300

    Initial Commit

---
+++ ExamApp/ExamAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamAppApp.swift
+//  ExamApp
+//
+//  Created by Карим on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ExamAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
